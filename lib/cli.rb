05c817a3d9b85107eab47c4fbb2faf66734fe3eb
Pascal Rassaby <pasbynumbers@gmail.com>
2019-05-30 18:26:36 +0100

    new_review method fixed

--- lib/cli.rb
+++ lib/cli.rb
@@ -8,7 +8,6 @@ class CommandLineInterface
     @current_user = nil
   end
 
-
   @@user = nil
 
   def greet
@@ -80,48 +79,72 @@ class CommandLineInterface
   end
 
   def user_options
-    puts "_____________"
+    puts '_____________'
     puts 'What would you like to do next?'
-    puts "_____________"
+    puts '_____________'
     puts "Search through Apps? Type 'choose_app'"
     puts "Review an App? Type 'review_app'"
     puts "Check your reviews by typing 'my_reviews'"
     puts "Search reviews per App by typing 'find_reviews'"
     response = gets.chomp
-      if response == 'choose_app'
-        choose_app
-      elsif response == 'review_app'
-        review_app
-      elsif response == 'my_reviews'
-        my_reviews
-      elsif response == 'find_reviews'
-        find_reviews
-      end
-
+    if response == 'choose_app'
+      choose_app
+    elsif response == 'review_app'
+      review_app
+    elsif response == 'my_reviews'
+      my_reviews
+    elsif response == 'find_reviews'
+      find_reviews
+    end
   end
 
   def review_app
+    app_rating_total = []
     new_review = Review.new
     puts "Type the name of the App you'd like to review"
     app_choice = gets.chomp
+    current_app = App.find_by_name(app_choice)
     puts "You have chosen to review #{app_choice}"
-    puts "Please write your review"
+    puts 'Please write your review'
     new_review_content = gets.chomp
-    puts "Please give a rating beween 1 and 5"
+    puts 'Please give a rating beween 1 and 5'
     new_review_rating = gets.chomp
     puts "This is what you've submitted so far"
     puts app_choice
     puts new_review_content
     puts "Rating: #{new_review_rating}"
-    puts "Would you like to save this review?"
+    puts 'Would you like to save this review?'
     response = gets.chomp
     if response == 'Y'
       new_review.content = new_review_content
       new_review.rating = new_review_rating
       new_review.user_id = @current_user.id
-      new_review.app_id = App.find_by_name(app_choice).id
+      new_review.app_id = current_app.id
       new_review.save
+
+      # update User Records
+      @current_user.app_id = new_review.app_id
+      @current_user.review_id = new_review.id
+
+      # update App Records
+      current_app.user_id = @current_user.id
+      current_app.review_id = new_review.id
+
+      # update total review count in Apps
+      current_app.total_reviews += 1
+
+      # update avg rating in Apps
+      Review.all.select do |review|
+        app_rating_total << review.rating if review.app_id == new_review.app_id
+      end
+
+      new_avg = app_rating_total.sum / app_rating_total.size
+
+      current_app.avg_rating = new_avg
+      current_app.save
+      binding.pry
       puts 'Your review has been saved!'
+
     else
       puts 'Lets try again'
       review_app
@@ -129,54 +152,48 @@ class CommandLineInterface
     user_options
   end
 
-def find_reviews
-  puts "Which app's reviews would you like to check?"
-  response = gets.chomp
-  app_choice = App.find_by_name(response)
+  def find_reviews
+    puts "Which app's reviews would you like to check?"
+    response = gets.chomp
+    app_choice = App.find_by_name(response)
 
+    current_reviews = Review.where(app_id: app_choice.id)
+    puts '_______'
+    puts app_choice.name
+    print "Average Rating: #{app_choice.avg_rating}"
+    puts ' '
+    puts '_______'
 
-  current_reviews = Review.where(app_id: app_choice.id)
-  puts "_______"
-  puts app_choice.name
-  print "Average Rating: #{app_choice.avg_rating}"
-  puts " "
-  puts "_______"
+    current_reviews.map do |review|
+      User.all.select do |t|
+        next unless t.id == review.user_id
 
+        puts ' '
+        print 'Review: '
+        print review.content
+        print ' | '
+        print 'Rating: '
+        print review.rating
+        print ' | '
+        print 'User: '
 
-  current_reviews.map do |review|
-    User.all.select do |t|
-      if t.id == review.user_id
-    puts " "
-    print "Review: "
-    print review.content
-    print " | "
-    print "Rating: "
-    print review.rating
-    print " | "
-    print "User: "
-
-      puts t.name
+        puts t.name
+      end
+      puts ' '
+      puts ' '
     end
+      end
+
+  def debug?
+    puts 'Do you want to debug?'
+    response = gets.chomp
+    if response == 'Y'
+      user_options
+    else
+      puts "Let's carry on then"
+      new_user
     end
-    puts " "
-    puts " "
-
   end
-
-
-end
-
-
-def debug?
-  puts "Do you want to debug?"
-  response = gets.chomp
-  if response == 'Y'
-    user_options
-  else
-    puts "Let's carry on then"
-    new_user
-  end
-end
 
   def new_user
     @current_user = User.new
